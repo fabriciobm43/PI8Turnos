cf8413cf7fba080a3fe94399e48693f04dd0d62e
Jaime Fabricio Baño Mera <66963732+fabriciobm43@users.noreply.github.com>
2021-01-21 12:27:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pi8turnos/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pi8turnos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
